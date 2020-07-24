575e95373d74dcb6cebb1a21354ee8b4b9ea9dfa
mohit2512sharma <mohitlakshya@gmail.com>
2020-07-24 20:48:28 +0530

    initial testing with small function

---
+++ R/ds_to_json.R
@@ -0,0 +1,13 @@
+#' @title ds_to_json
+#' @description To convert a named list into a json data structure
+#' @param x A named list to be converted to json data structure
+#' @return a string in json format
+#' @examples
+#' @export
+#' @importFrom
+
+ds_to_json = function(x){
+  return(x)
+}
+
+
--- R/test.R
+++
@@ -1,7 +0,0 @@
-#' @title
-#' @description
-#' @param
-#' @return
-#' @examples
-#' @export
-#' @importFrom
